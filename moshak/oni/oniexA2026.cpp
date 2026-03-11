82b3cb66402be63e3adef1cc55ea7660b474d662
Miguel wheeler <dinossaurotop10@gmail.com>
2026-03-11 14:09:51 +0000

    Add files via upload

---
+++ moshak/oni/oniexA2026.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <locale.h>
+#include <cstdlib>
+#include <time.h>
+#include <cctype>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Portuguese");
+	srand(time(0));
+	int Testes, Nalunos, AltAlunos[50000], contador = 0;
+	string R[20];
+
+	cin >> Testes;
+
+	for (int i = 0; i < Testes; i++) {
+		contador = 0;
+		cin >> Nalunos;
+		for (int y = 0; y < Nalunos; y++) {
+			cin >> AltAlunos[y]," ";
+			
+		}
+		for (int y = 0; y < Nalunos - 1; y++) {
+			if (AltAlunos[y] > AltAlunos[y + 1]) {
+				contador++;
+			}
+		}	
+		if (contador >1)
+		{
+		
+		R[i] = "NAO";
+
+		}
+		else if (contador <= 1) {
+			R[i] = "SIM";
+		}
+
+	}
+
+	for (int z = 0; z < Testes; z++) {
+		cout << R[z];
+	}
+
+}
